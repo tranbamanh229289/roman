4d2ca071dded55881506c1c4680cc324c45d5af0
tranbamanh229289 <tranbamanhlovevietnam@gmail.com>
2026-04-30 03:09:23 +0700

    subseqmax

---
+++ src/problems/arrays/subseqmax.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int solve(vector<int>& nums) {
+  int max = nums[0];
+  int current = nums[0];
+
+  for(int i = 1; i < nums.size(); i ++) {
+    current = current + nums[i] >= nums[i] ? current + nums[i] : nums[i]; 
+    max = max > current ? max : current;
+  }
+  return max;
+}
+
+void run(){
+  vector<int> a = {-2, 11, -4, 13, -5, 2};
+  int result = solve(a);
+  cout<<result;
+}
--- src/problems/string/longestsubstringwithoutrepeatingchar.cpp
+++ src/problems/string/longestsubstringwithoutrepeatingchar.cpp
@@ -4,26 +4,22 @@
 using namespace std;
 
 int solve1(string s) {
-  int max = 0;
+  int max_len = 0;
   int current = 0;
   int start = 0;
 
-unordered_map<char, int> map;
+  unordered_map<char, int> map;
 
   for(int i = 0; i < s.size(); i ++) {
     char c = s[i];
 
     if(map.find(c) != map.end() && start <= map[c]) {
-      current = current - map[c] + start;
       start = map[c]+1;
-    } else {
-      current += 1;
     }
-    max= max > current ? max: current;
-  
     map[c] = i;
+    max_len = max(max_len, i - start + 1);
   }
-  return max;
+  return max_len;
 }
 
 void run() {
